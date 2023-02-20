2015e586bbbecb32ed46ffbc60b39b33ba62a1a1
kdhingra07 <kdhingra@twilio.com>
2023-02-20 15:38:03 +0530

    added quantcast objective C module

---
+++ Headers/QuantcastSPM-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  
+//
+//  Created by Komal Dhingra on 20/02/23.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <QuantcastMeasurement.h>
+
+#endif /* Header_h */
---
+++ Sources/QuantcastSPM/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Komal Dhingra on 20/02/23.
+//
+
+import Foundation
